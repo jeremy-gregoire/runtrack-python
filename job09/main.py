49d5342f082022745ae7e73c5aa4642fb5e4d561
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-03-04 18:38:20 +0100

    Job09 a changé du à la mal compréhension du TP

--- job09/main.py
+++ job09/main.py
@@ -1,4 +1,4 @@
-# La classe produit pour créer des produits
+# Une classe pour créer des produits
 class Produit:
   def __init__(self,nom,prix_unitaire,quantite_en_stock) -> None:
     self.nom=nom
@@ -6,17 +6,33 @@ class Produit:
     self.quantite_en_stock=quantite_en_stock
   
   def afficher(self) -> None:
-    print("Nom du produit: "+self.nom+"\nPrix unitaire: "+str(self.prix_unitaire)+"\nQuantité en stock: "+str(self.quantite_en_stock))
+    print("Nom du produit: " + self.nom+"\nPrix unitaire: " + str(self.prix_unitaire) + "\nQuantité en stock: " + str(self.quantite_en_stock))
 
-# Le produit à afficher
-mon_produit=Produit("Cassoulet", 2_000_000.45, 2_000)
-
-# Affiche les informations du produits par défaut
+print("==================== PRODUIT ACTUEL ====================")
+# Crée et affiche un produit
+mon_produit = Produit("Cassoulet", 5.45, 2_000)
 mon_produit.afficher()
 
-# Applique l'inflation au produit
-print("===================== INFLATION =====================")
-mon_produit.prix_unitaire*=1.1
+print("==================== AJOUT D'UN PRODUIT EN STOQUE ====================")
+# Ajout d'un produit en stock et affiche le produit mis à jour
+mon_produit.quantite_en_stock += 1
+mon_produit.afficher()
 
-# Affiche les informations du produits avec les modifications de l'inflation
+print("==================== ACHETER LE PRODUIT ====================")
+# Demande à l'utilisateur de saisir une quantité de produit qu'il souhaite acheter
+quantite_achete_par_utilisateur = int(input("Combien voulez acheter ? "))
+
+# Mets à jour les stocks du produit
+mon_produit.quantite_en_stock -= quantite_achete_par_utilisateur
+mon_produit.afficher()
+
+# Affiche un bilan d'achat
+print("==================== BILAN DE L'ACHAT ====================")
+print("Quantité acheter: " + str(quantite_achete_par_utilisateur))
+print("Prix unitaire du produit: " + str(mon_produit.prix_unitaire))
+print("Prix total à payer: " + str(quantite_achete_par_utilisateur * mon_produit.prix_unitaire))
+
+print("==================== LE PRIX DU PRODUIT AUGMENTE DE 10% ====================")
+# Augmentation de 10% le prix du produit du à l'inflation
+mon_produit.prix_unitaire *= 1.1
 mon_produit.afficher()
